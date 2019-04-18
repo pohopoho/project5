c39c333ea1e215725c3a5d8bfe2a074ff82a91d8
pohopoho <49660820+pohopoho@users.noreply.github.com>
2019-04-18 17:00:52 -0400

    Add files via upload

--- LList.java
+++ LList.java
@@ -9,6 +9,8 @@
  */
 package prj5;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 // import org.w3c.dom.Node;
 // import linkedlist.SinglyLinkedList;
 // import linkedlist.SinglyLinkedList.Node;
@@ -25,176 +27,192 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
 
     private Node head;
     private Node tail;
-    private int size;
-    private Node current;
+    private int numberOfEntries;
 
 
     public LList() {
-        head = new Node(null);
-        tail = new Node(null);
-        size = 0;
-        head.setNext(tail);
+        head = null;
+        tail = null;
+        numberOfEntries = 0;
+
     }
 
 
     @Override
     public void add(T anEntry) {
-        add(size+1, anEntry);
+        Node newNode = new Node(anEntry);
+        if (isEmpty()) {
+            head = newNode;
+        }
+        else {
+            tail.setNext(newNode);
+            tail = newNode;
+            
+        }
+        numberOfEntries++;
     }
 
 
     @Override
     public void add(int position, T anEntry) {
-        if ((position >= 0) && (position <= size + 1)) {
-            Node newEntry = new Node(anEntry);
-            if (position == 1) {
-                head.setNext(newEntry);
+        if ((position >= 1) && (position <= numberOfEntries + 1)) {
+            Node newNode = new Node(anEntry);
+            if (isEmpty())
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else if (position == 1) {
+                newNode.setNext(head);
+                head = newNode;
+            }
+            else if(position == numberOfEntries)
+            {
+                tail.setNext(newNode);
+                tail = newNode;
             }
             else {
-                current = head;
-                for (int i = 1; i < position; i++) {
-                    current = current.nextNode;
-                }
-                Node nodeBefore = current;
-                Node nodeAfter = current.nextNode;
-                nodeBefore.setNext(newEntry);
-                newEntry.setNext(nodeAfter);
+                Node before = getNodeAt(position - 1);
+                Node after = before.getNext();
+                newNode.setNext(after);
+                before.setNext(newNode);
             }
-            size++;
+            numberOfEntries++;
         }
         else {
-            throw new IndexOutOfBoundsException(
-                "Position for new entry is invalid:/ Please try again!");
+            throw new IndexOutOfBoundsException();
         }
     }
 
 
     @Override
     public void clear() {
-        head.nextNode = null;
-        size = 0;
+        head = null;
+        tail = null;
+        numberOfEntries = 0;
     }
 
 
     @Override
     public boolean contains(T anEntry) {
-        if (anEntry == null || isEmpty()) {
-            return false;
-        }
-        current = head.nextNode;
-        while (current != null) {
+        boolean found = false;
+        Node current = head;
+        while (!found && (current != null)) {
             if (current.data.equals(anEntry)) {
-                return true;
+                found = true;
             }
-            current = current.nextNode;
+            else {
+                current = current.getNext();
+            }
+
         }
-        return false;
+        return found;
     }
 
 
     @Override
     public T getEntry(int position) {
-        current = head.nextNode;
-        if (position == 0) {
-            return current.getData();
+        if ((position >= 1) && (position <= numberOfEntries)) {
+            assert !isEmpty();
+            return getNodeAt(position).getData();
         }
-        else if (position < 0 || position >= size) {
-            throw new IndexOutOfBoundsException(
-                "Position entered is invalid:/ Please try again!");
-        }
-        else {  
-            for (int i = 0; i < position; i++) {
-                current = current.nextNode;
-            }
-            return current.getData();
+        else {
+            throw new IndexOutOfBoundsException();
         }
     }
 
 
     @Override
     public int getLength() {
-        return size;
+        return numberOfEntries;
     }
 
 
     @Override
     public boolean isEmpty() {
-        return size == 0;
+        return numberOfEntries == 0;
     }
 
 
     @Override
-    public T remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException(
-                "Illegal index value! Please enter a valid index value.");
-        }
-        current = head.nextNode;
-        /*if (index == 0) {
-            return current.nextNode.getData();
-        }*/
-        for (int i = 0; i < index; i++) {
-            current = current.nextNode;
-        }
-        Node result = current.nextNode;
-        current.nextNode = current.nextNode.nextNode;
-        size--;
-        return result.getData(); 
-    }
-
-
-    public boolean remove(T anEntry) {
-        if (anEntry == null || isEmpty()) {
-            return false;
-        }
-        current = head.nextNode;
-        if (contains(anEntry)) {
-            while (current != null) {
-                if (current.getData().equals(anEntry)) {
-                    current.setNext(current.nextNode.nextNode);
-                    return true;
+    public T remove(int position) {
+        T result = null;
+        if ((position >= 1) && (position <= numberOfEntries)) {
+            assert !isEmpty();
+            if (position == 1) {
+                result = head.getData();
+                head = head.getNext();
+                if(numberOfEntries == 1)
+                {
+                    tail = null;
                 }
-                current = current.nextNode;
             }
+            else {
+                Node before = getNodeAt(position - 1);
+                Node toRemove = before.getNext();
+                Node after = toRemove.getNext();
+                result = toRemove.getData();
+                before.setNext(after);
+                if(position == numberOfEntries)
+                {
+                    tail = before;
+                }
+            }
+            numberOfEntries--;
+            return result;
         }
-        return false;
-    }
+        else {
+            throw new IndexOutOfBoundsException();
+        }
 
+    }
 
     @Override
-    public T replace(int index, T anEntry) {
-        if (anEntry == null || index < 0 || index > size) {
-            throw new IllegalArgumentException(
-                "Invalid argument! Please pass a valid argument.");
+    public T replace(int position, T anEntry) {
+        if ((position >= 1) && (position <= numberOfEntries)) {
+            assert !isEmpty();
+            Node desiredNode = getNodeAt(position);
+            T originalEntry = desiredNode.getData();
+            desiredNode.setData(anEntry);
+            return originalEntry;
         }
-        current = head.nextNode;
-        int counter = 0;
-        while(counter != index)
-        {
-            current = current.nextNode;
-            counter++;
+        else {
+            throw new IndexOutOfBoundsException();
         }
-        T result = current.getData();
-        current.setData(anEntry);
-        return result;  
     }
- 
- 
+
+
     @Override
     public Object[] toArray() {
-        if (isEmpty()) {
-            throw new IllegalStateException("The queue is empty");
+        @SuppressWarnings("unchecked")
+        T[] result = (T[])new Object[numberOfEntries];
+        int index = 0;
+        Node current = head;
+        while ((index < numberOfEntries) && current != null)
+        {
+            result[index] = current.getData();
+            current = current.getNext();
+            index++;
         }
-        Object[] arr = new Object[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = getEntry(i);
-        }
-        return arr;
+        return result;
     }
 
 
     public int size() {
-        return size; 
+        return numberOfEntries;
+    }
+
+
+    private Node getNodeAt(int index) {
+        assert (head != null) && (1 <= index) && (index <= numberOfEntries);
+
+        Node curr = head;
+        int counter = 0;
+        while (counter != index && curr.getNext() != null) {
+            curr = curr.getNext();
+            counter++;
+        }
+        return curr;
     }
 
 
@@ -210,12 +228,13 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
             LList<T> other = ((LList<T>)obj);
             if (other.size() == this.size()) {
                 Node otherCurrent = other.head;
+                Node current = head;
                 while (current != null) {
                     if (!current.getData().equals(otherCurrent.getData())) {
                         return false;
                     }
-                    current = current.nextNode;
-                    otherCurrent = otherCurrent.nextNode;
+                    current = current.getNext();
+                    otherCurrent = otherCurrent.getNext();
                 }
                 return true;
             }
@@ -248,6 +267,47 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         public void setData(T anEntry) {
             data = anEntry;
         }
+        
+        public Node getNext()
+        {
+            return nextNode;
+        }
     }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LListIterator();
+    }
+
+    private class LListIterator implements Iterator<T> {
+        
+        private Node curr;
+        
+        public LListIterator()
+        {
+            curr = new Node(null);
+            curr.setNext(head);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (curr.nextNode != null);
+        }
+        
+        
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T temp = curr.getData();
+            curr = curr.getNext();
+            return temp;
+        }
+
+    }
+
 
 }
