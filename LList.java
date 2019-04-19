71ec2dad3deb6dad2c2ad28a5a9c03bf1b0f3353
pohopoho <49660820+pohopoho@users.noreply.github.com>
2019-04-19 10:32:53 -0400

    Add files via upload

--- LList.java
+++ LList.java
@@ -11,9 +11,6 @@ package prj5;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-// import org.w3c.dom.Node;
-// import linkedlist.SinglyLinkedList;
-// import linkedlist.SinglyLinkedList.Node;
 import list.ListInterface;
 
 /**
@@ -29,7 +26,9 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
     private Node tail;
     private int numberOfEntries;
 
-
+    /**
+     * Constructor
+     */
     public LList() {
         head = null;
         tail = null;
@@ -37,7 +36,10 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
 
     }
 
-
+    /**
+     * adds an entry to the list
+     * @param anEntry the entry to be added
+     */
     @Override
     public void add(T anEntry) {
         Node newNode = new Node(anEntry);
@@ -53,7 +55,11 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         numberOfEntries++;
     }
 
-
+    /**
+     * adds and entry to the list at a specified position, if position is illegal, throws an IndexOutOfBoundsException
+     * @param position the position to be added at
+     * @param anEntry the entry to be added
+     */
     @Override
     public void add(int position, T anEntry) {
         if ((position >= 1) && (position <= numberOfEntries + 1)) {
@@ -85,7 +91,9 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         }
     }
 
-
+    /**
+     * clears the list
+     */
     @Override
     public void clear() {
         head = null;
@@ -93,7 +101,11 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         numberOfEntries = 0;
     }
 
-
+    /**
+     * checks if the list contains a given entry
+     * @param anEntry the entry to be added
+     * @return true if entry is in the list, false if not
+     */
     @Override
     public boolean contains(T anEntry) {
         boolean found = false;
@@ -110,7 +122,11 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         return found;
     }
 
-
+    /**
+     * retrieves an entry at a given position
+     * @param position the position whose entry is to be returned
+     * @return the entry at the given position, if not found, throws a IndexOutOfBoundsException
+     */
     @Override
     public T getEntry(int position) {
         if ((position >= 1) && (position <= numberOfEntries)) {
@@ -122,19 +138,29 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         }
     }
 
-
+    /**
+     * accessor for list length(numberOfEntries)
+     * @return the length(numberOfEntries) of the list
+     */
     @Override
     public int getLength() {
         return numberOfEntries;
     }
 
-
+    /**
+     * checks if the list is empty
+     * @return true if empty, false if not empty
+     */
     @Override
     public boolean isEmpty() {
         return numberOfEntries == 0;
     }
 
-
+    /**
+     * removes an entry in the list at a given position
+     * @param position the position whose entry to be removed
+     * @return the removed entry, if position is illegal, throws IndexOutOfBoundsException
+     */
     @Override
     public T remove(int position) {
         T result = null;
@@ -168,6 +194,12 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
 
     }
 
+    /**
+     * replaces the data in the node at a given position with a new given data (entry)
+     * @param position the position at which to replace node's data
+     * @param anEntry the data to replace with
+     * @return the replaced data, if position is illegal, throws IndexOutOfBoundsException
+     */
     @Override
     public T replace(int position, T anEntry) {
         if ((position >= 1) && (position <= numberOfEntries)) {
@@ -182,7 +214,10 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         }
     }
 
-
+    /**
+     * creates and returns an array representation of the list
+     * @return the array representation of the list
+     */
     @Override
     public Object[] toArray() {
         @SuppressWarnings("unchecked")
@@ -198,12 +233,11 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         return result;
     }
 
-
-    public int size() {
-        return numberOfEntries;
-    }
-
-
+    /**
+     * helper method to retrieve a node at a given position
+     * @param index the index to find a node at
+     * @return the node at the given index
+     */
     private Node getNodeAt(int index) {
         assert (head != null) && (1 <= index) && (index <= numberOfEntries);
 
@@ -216,7 +250,11 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         return curr;
     }
 
-
+    /**
+     * compares this object with another object
+     * @param obj the object to be compared to
+     * @return true if equal, false if not equal
+     */
     public boolean equals(Object obj) {
         if (obj == this) {
             return true;
@@ -227,7 +265,7 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         if (this.getClass() == obj.getClass()) {
             @SuppressWarnings("unchecked")
             LList<T> other = ((LList<T>)obj);
-            if (other.size() == this.size()) {
+            if (other.getLength() == this.getLength()) {
                 Node otherCurrent = other.head;
                 Node current = head;
                 while (current != null) {
@@ -244,7 +282,11 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         return false;
     }
 
-
+    /**
+     * Inner node class
+     * @author Michael Cheung
+     *
+     */
     private class Node {
         private T data;
         private Node nextNode;
@@ -275,12 +317,20 @@ public class LList<T> implements ListInterface<T>, Iterable<T> {
         }
     }
 
-
+    /**
+     * creates an Iterator for this list
+     * @return a new Iterator
+     */
     @Override
     public Iterator<T> iterator() {
         return new LListIterator();
     }
 
+    /**
+     * Iterator inner class
+     * @author Michael Cheung
+     *
+     */
     private class LListIterator implements Iterator<T> {
         
         private Node curr;
