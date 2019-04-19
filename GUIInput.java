30653cefb24975b593748aa83a82fcf8ac851652
natjac99 <49663098+natjac99@users.noreply.github.com>
2019-04-19 18:30:50 -0400

    Update GUIInput.java

--- GUIInput.java
+++ GUIInput.java
@@ -8,16 +8,24 @@ import java.text.ParseException;
 
 /**
  * @author Michael Cheung
- * @version
- *
+ * @version 4.19.2019
+ *This is the GUIInput class. This class contains the main method for the project.
  */
 public class GUIInput {
-    
+    /**
+     * This is the GUIInput() constructor.
+     */
     public GUIInput()
     {
-        
+        //This is supposed to be empty
     }
     
+    /**
+     * This is the main method for GUIInput class.
+     * @param args this is the parameter args
+     * @throws FileNotFoundException this exception is thrown when file is not found
+     * @throws ParseException this is a parse exception
+     */
     public static void main(String[] args) throws FileNotFoundException, ParseException
     {
         InputReader reader = new InputReader("MusicSurveyData2018.csv", "SongList2018.csv");
