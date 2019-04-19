2e051be2f87e98e7da096149e146c70a62100c61
Daniel Moreno <47113619+danielrm8@users.noreply.github.com>
2019-04-19 17:42:48 -0400

    SorterTest with assertions

--- SorterTest.java
+++ SorterTest.java
@@ -1,16 +1,93 @@
 /**
- * 
+ * Virginia Tech Honor Code Pledge:
+ *
+ * As a Hokie, I will conduct myself with honor
+ * and integrity at all times.
+ * I will not lie, cheat, or steal, nor will I
+ * accept the actions of those who do.
+ * -- Daniel Moreno (danielrm)
  */
 package prj5;
 
 import student.TestCase;
 
 /**
- * @author Michael Cheung
+ * Test class for Sorter.
  *
+ * @author Daniel Moreno (danielrm)
+ * @version 04/16/2019
  */
+public class SorterTest extends TestCase {
+    private Sorter sorter;
+    private LList<Song> songs;
 
-public class SorterTest extends TestCase{
+    /**
+     * Setup for the test methods
+     */
+    public void setUp() {
+        Song songs1 = new Song("titleB", "artistB", 1990, "genreB");
+        Song songs2 = new Song("titleC", "artistC", 2000, "genreC");
+        Song songs3 = new Song("titleD", "artistD", 2010, "genreD");
+        Song songs4 = new Song("titleA", "artistA", 1980, "genreA");
+        songs = new LList<Song>();
+        songs.add(songs1);
+        songs.add(songs2);
+        songs.add(songs3);
+        songs.add(songs4);
+        
+        sorter = new Sorter(songs);
+    }
+
+
+    /**
+     * Tests the sortByArtist method
+     */
+    public void testSortByArtist() {
+        sorter.sortByArtist();
+        assertEquals(songs.getEntry(1).getArtist(), "artistA");
+        assertEquals(songs.getEntry(2).getArtist(), "artistB");
+        assertEquals(songs.getEntry(3).getArtist(), "artistC");
+        assertEquals(songs.getEntry(4).getArtist(), "artistD");
+    }
+
+
+    /**
+     * Tests the sortByTitle method
+     */
+    public void testSortByTitle() {
+        sorter.sortByTitle();
+        assertEquals(songs.getEntry(1).getTitle(), "titleA");
+        assertEquals(songs.getEntry(2).getTitle(), "titleB");
+        assertEquals(songs.getEntry(3).getTitle(), "titleC");
+        assertEquals(songs.getEntry(4).getTitle(), "titleD");
+    }
+
+
+    /**
+     * Tests the sortByYear method
+     */
+    public void testSortByYear() {
+        sorter.sortByYear();
+        assertEquals(songs.getEntry(1).getYear(), 1980);
+        assertEquals(songs.getEntry(2).getYear(), 1990);
+        assertEquals(songs.getEntry(3).getYear(), 2000);
+        assertEquals(songs.getEntry(4).getYear(), 2010);
+    }
+
+
+    /**
+     * Tests the sortByGenre method
+     */
+    public void testSortByGenre() {
+        sorter.sortByGenre();
+        assertEquals(songs.getEntry(1).getGenre(), "genreA");
+        assertEquals(songs.getEntry(2).getGenre(), "genreB");
+        assertEquals(songs.getEntry(3).getGenre(), "genreC");
+        assertEquals(songs.getEntry(4).getGenre(), "genreD");
+    }
+}
+
+/*public class SorterTest extends TestCase{
     
     private LList<Song> songList;
     private Sorter sort;
@@ -49,4 +126,4 @@ public class SorterTest extends TestCase{
         sort.sortByGenre();
     }
 
-}
+}*/
